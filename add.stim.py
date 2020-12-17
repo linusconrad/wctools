1a3b1747771db760e9c8fd077ea0f47a612d6ebe
linus Johann Conrad <18187684+linusconrad@users.noreply.github.com>
2020-12-17 17:08:58 +0000

    added a working tidier in python

--- add.stim.py
+++ add.stim.py
@@ -1,16 +1,26 @@
 # This script creates a stimulus waveform of an .abf file with the pyabf API
 
 import pyabf
-import pandas
+import pandas as pd
 
-abf = pyabf.ABF("2020_12_15 sgn P7 2DIV 1B cell5_0004.abf")
-
-print(abf.headerText)
-
+def tidyabf(file):
+    abf = pyabf.ABF(file)
 # Make a container for the data
-stimulus = []
-
+    abfcontents = []
 # iterate over the sweeps to generate all the stimulus waveform
-for i in range(abf.sweepCount):
-    abf.setSweep(i)
-    
+    for i in range(abf.sweepCount):
+        abf.setSweep(i)
+        loopdf = pd.DataFrame({'t': pd.Series(abf.sweepX),
+                              'value': pd.Series(abf.sweepY),
+                              'command' : pd.Series(abf.sweepC)})
+        abfcontents.append(loopdf)
+    # collect the data to one frame
+    abfdata = pd.concat(abfcontents)
+    abf.sweepPointCount
+    # add sweep information
+    sweep = pd.Series(abf.sweepList).repeat(abf.sweepPointCount)
+
+    # need to use "values" here else pandas get problems with indices
+    abfdata['sweep'] = sweep.values
+
+    return(abfdata)
