c51ad3450929f19c8c652b95fbf89ab16e1bb41f
linusconrad <18187684+linusconrad@users.noreply.github.com>
2022-08-05 10:17:48 +0100

    changed modelling to simple self start asymtote (works so much better!)

--- R/VCKslow.R
+++ R/VCKslow.R
@@ -71,7 +71,7 @@ process.VCstep =
       left_join(VCdatabysweep, peakdata)
     
     # write to file in case TC analysis fails  
-    readr::write_csv(VCdatabysweep, file = paste0(abffile, "VCmeasurements.csv"))
+   readr::write_csv(VCdatabysweep, file = paste0(abffile, "VCmeasurements.csv"))
     
     # Boltzmann fit
     fit = minpack.lm::nlsLM(
@@ -106,81 +106,33 @@ process.VCstep =
    # Fit a biexponential to everything
     safefit = purrr::safely(minpack.lm::nlsLM)
     
-    # KAfits =
-    #   VCfitdata %>%
-    #   group_by(.data$Vm, .data$sweep) %>%
-    #   nest() %>%
-    #   mutate(fit = purrr::map(.data$data, ~ safefit(
-    #     formula = Inorm ~ SSbiexp(tfit, A1, lrc1, A2, lrc2),
-    #     data = .
-    #   )))
-    # 
-    # # unpack the results
-    # KAfits$fit = purrr::transpose(KAfits$fit)$result
-
-    # Also try conventional approach with starting value to see if it performs better
-    KAfits2 =
+    KAfits =
       VCfitdata %>%
       group_by(.data$Vm, .data$sweep) %>%
       nest() %>%
-      mutate(fit = purrr::map(
-        .data$data,
-        ~ safefit(
-          formula = Inorm ~ A1 * exp(-tfit / tau1) + (1-A1) * exp(-tfit / tau2),
-          data = .,
-          minpack.lm::nls.lm.control(maxiter = 10000),
-          start = list(
-            tau1 = 0.080,
-            tau2 = 0.250,
-            A1 = 0.8),
-          upper = c(
-            tau1 = 0.150,
-            tau2 = 1.6,
-            A1 = 1
-          ),
-          lower = c(
-            tau1 = 0.001,
-            tau2 = 0.150,
-            A1 = 0
-          )
-        )
-      ))
-    
+      mutate(fit = purrr::map(.data$data, ~ safefit(
+        formula = Inorm ~ SSasymp(tfit, A1, lrc1, A2),
+        data = .
+      )))
+
     # unpack the results
-    KAfits2$fit = purrr::transpose(KAfits2$fit)$result
-    
-  
-    
-    # KAfits = 
-    # KAfits %>%
-    #   mutate(
-    #     params = purrr::map(.data$fit, broom::tidy),
-    #     KAcurves = purrr::map(.data$fit, generics::augment, newdata = tibble(tfit = seq(0.02, 0.55, 0.01)))
-    # )
-    
-    KAfits2 = 
-      KAfits2 %>%
+    KAfits$fit = purrr::transpose(KAfits$fit)$result
+
+    KAfits =
+      KAfits %>%
       mutate(
         params = purrr::map(.data$fit, broom::tidy),
-        QC = purrr::map(.data$fit, broom::glance),
         KAcurves = purrr::map(.data$fit, generics::augment, newdata = tibble(tfit = seq(0.02, 0.55, 0.01)))
       )
     
+   # option for offline-use of the analysis (plotting)
     if (listobj == T) {
       return(list(VCdata,
                   VCfitdata,
                   KAfits2,
                   VCdatabysweep))
-    }
-    
-    
-    # Filter out non-converged
-    KAfits2 =
-      KAfits2 %>%
-      unnest(QC) %>%
-      filter(isConv == TRUE)
-
-    # Plot of the raw tc fits
+      }
+   # Plot of the raw tc fits
     TCfit =
       VCfitdata %>%
       ggplot(aes(x = .data$tfit, y = .data$Inorm)) +
@@ -193,17 +145,10 @@ process.VCstep =
                   aes(group = sweep),
                   formula = y ~ SSasymp(x,A1, lrc1, A2),
                   se = F,
-                  colour = "red") +
-      geom_line(
-        data = unnest(KAfits2, .data$KAcurves),
-        aes(y = .data$.fitted),
-        colour = "blue",
-        size = 0.5,
-        alpha = 0.5
-      )
+                  colour = "red") 
     
     KAparams =
-      KAfits2 %>%
+      KAfits %>%
       group_by(.data$sweep, .data$Vm) %>%
       select(.data$params) %>%
       unnest(.data$params) %>%
@@ -211,38 +156,32 @@ process.VCstep =
       pivot_wider(names_from = .data$term,
                   values_from = .data$estimate,
                   names_prefix = "IKA.") %>%
-      # rescale to ms time constants
-      mutate(IKA.tau1 = .data$IKA.tau1 * 1000,
-             IKA.tau2 =.data$IKA.tau2 * 1000)
+      # rescale from log rate constant to time constant in ms
+      mutate(IKA.tau1 = (1/exp(.data$IKA.lrc1)) * 1000)
     
     IKAparamplot =
       KAparams %>%
       group_by(.data$sweep, .data$Vm) %>%
-      select(.data$IKA.A1, .data$IKA.tau1, .data$IKA.tau2) %>%
-      pivot_longer(cols = c(3:5)) %>%
-      mutate(var = stringr::str_sub(.data$name, start = 5, end = 5)) %>%
-      ggplot(aes(x = .data$Vm, y = .data$value, colour = .data$name)) +
+      
+      ggplot(aes(x = .data$Vm,
+                 y = .data$IKA.tau1)) +
       geom_hline(yintercept = 0,
                  linetype = 3,
                  colour = "grey50") +
-      facet_wrap(
-        ~ .data$var,
-        scales = "free",
-        labeller = labeller(var = c(A = "Component", t = "Time constant, ms")),
-        strip.position = "left"
-      ) +
       geom_point() +
-      ggthemes::scale_color_solarized(accent = "blue") +
+      geom_line(linetype = 3) +
       theme(axis.title.y = element_blank(),
-            legend.title = element_blank())
+      legend.title = element_blank())
+    
+    #IKAparamplot
+    
     
     # Add KA fits to sweep summary
-    KAfitsfile =
-      KAparams %>%
-      select(.data$IKA.A1, .data$IKA.tau1, .data$IKA.tau2)
-    
-    
-    
+    VCdatabysweep =
+      VCdatabysweep |>
+      left_join(KAparams |> dplyr::select(.data$IKA.tau1))
+ 
+     
     # create summary plots
     # IV
     plot1 =
@@ -275,7 +214,7 @@ process.VCstep =
       ) +
       theme(#legend.title = element_blank(),
         panel.background = element_rect(fill = "grey90"))
-    
+
     # tails
     plot2 = ggplot(VCdatabysweep, aes(x = .data$V, y = .data$normtail)) +
       labs(y = "I tail, norm.",
@@ -286,17 +225,17 @@ process.VCstep =
       geom_point() +
       annotate(geom = "text",label = paste0("Vh =", plyr::round_any(tidyfit$estimate[1],0.1)), x = 0, y = 0.5, colour = "deeppink") +
       geom_line(data = generics::augment(fit), aes(y = .data$.fitted), colour = "deeppink")
-    
-    
+
+
     # peaks
-    plot3 = 
+    plot3 =
       VCdata %>%
       filter(.data$t < tjump + 0.03, .data$t > tjump + 0.0002) %>%
       ggplot(aes(x = .data$t - tjump, y = .data$Imemb)) +
       geom_line(aes(group = .data$sweep)) +
       geom_point(data = VCdatabysweep, aes(y = .data$Ipeak.in, x = .data$tpeak.in ), colour = "red") +
-      geom_point(data = VCdatabysweep, aes(y = .data$Ipeak.out, x = .data$tpeak.out ), colour = "blue") 
-    
+      geom_point(data = VCdatabysweep, aes(y = .data$Ipeak.out, x = .data$tpeak.out ), colour = "blue")
+
     plot4 =
       ggplot(VCdatabysweep, aes(x = .data$V, y = .data$Ipeak.in)) +
       geom_line(colour = "red", alpha = 0.2) +
@@ -310,8 +249,8 @@ process.VCstep =
                  colour = "grey50",
                  xintercept = 0) +
       geom_point(colour = "red")
-    
-    
+
+
     plot5 =
       ggplot(VCdatabysweep, aes(x = .data$V, y = .data$Ipeak.out)) +
       geom_line(colour = "blue", alpha = 0.2) +
@@ -325,7 +264,7 @@ process.VCstep =
                  colour = "grey50",
                  xintercept = 0) +
       geom_point(colour = "blue")
-    
+
     # delay of peak
     plot6 =
       VCdatabysweep %>%
@@ -342,15 +281,14 @@ process.VCstep =
       geom_point() +
       scale_colour_manual(values = c("red", "blue")) +
       geom_line(alpha = 0.2)
-    
+    # 
     #make the layout
     (bigplot =
         (plot0 + plot1) / (TCfit + IKAparamplot) /(plot2 + plot6) + patchwork::plot_annotation(title = "VC Slow K currents",
                                                                                             subtitle = paste0(abffile, "\n Analysed on ", Sys.Date())))
-    # write to file
-    
-    
-    readr::write_csv(KAfitsfile, file = paste0(abffile, "IKA-TCfitting.csv"))
-    ggsave(bigplot, width = 11, height = 11, filename = paste0(abffile, ".VCsteps.png"))
-    return(bigplot)
+    # write to file (overwrite)
+    readr::write_csv(VCdatabysweep, file = paste0(abffile, "VCmeasurements.csv"))
+    # 
+     ggsave(bigplot, width = 11, height = 11, filename = paste0(abffile, ".VCsteps.png"))
+     return(bigplot)
   }
